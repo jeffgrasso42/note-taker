32e63c06417b127e466430aaa740df100b8c0761
JAG <jagrasso42@gmail.com>
2022-10-27 18:57:24 -0400

    added express server

---
+++ server.js
@@ -0,0 +1,13 @@
+// DEPENDENCIES
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middlewares
+
+// routes
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
