c0ceb4e6beb656f006981390e10325cec94ec775
JAG <jagrasso42@gmail.com>
2022-10-27 21:57:49 -0400

    added DELETE request to remove notes from db.json

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -1,20 +1,45 @@
 // DEPENDENCIES
 const router = require('express').Router();
 const fs = require('fs');
+// psuedodatabase
 const notes = require('../../db/db.json');
+// helper function to generate random ids
+const uuid = require('../../helpers/uuid');
 
 // GET / - notes from db.json
 router.get('/', (req, res) => res.json(notes));
 
 // POST / - add notes to db.json
 router.post('/', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-  notes.push(req.body);
-  console.info(notes);
+  // store object for alteration
+  const newNote = req.body;
+  // add random id to object
+  newNote.id = uuid();
+  // add updated object to notes array
+  notes.push(newNote);
+
+  // updated db.json file
   fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, 2), err =>
     err ? console.error(err) : console.info(`Note for ${req.body.title} has been written to JSON file`)
   );
+});
+
+// DELETE / - remove notes from db.json
+router.delete('/:id', (req, res) => {
+  if (req.body) {
+    console.info(`${req.method} request received to delete a note`);
+    notes.forEach((note, idx) => {
+      if (note.id === req.params.id) {
+        notes.splice(idx, 1);
+        return;
+      }
+    });
+
+    fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, 2), err =>
+      err ? console.error(err) : console.info(`Note with id: ${req.params.id} has been removed from JSON file`)
+    );
+    res.status(200);
+  }
 });
 
 module.exports = router;
